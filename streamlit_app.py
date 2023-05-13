497885ba222064664d68db74af71f8c1c4aaf2c2
aabaaji2012 <133515912+aabaaji2012@users.noreply.github.com>
2023-05-13 20:28:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My Parents new heathy diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Samdwitch')
-streamlit.text('Omlette Bread')
-streamlit.text('Honey Pie')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗  Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
