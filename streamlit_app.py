65cca194074aba51dbf2dbd44504c355cda88adb
aabaaji2012 <133515912+aabaaji2012@users.noreply.github.com>
2023-05-13 20:24:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new heathy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Samdwitch')
+streamlit.text('Omlette Bread')
+streamlit.text('Honey Pie')
